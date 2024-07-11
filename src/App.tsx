639ba044438297e9f5ab248fd4e3f6bef9d73def
J Friesen <jiffybugg2@gmail.com>
2024-07-11 11:24:31 -0600

    input field, import registration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import './App.css';
 
-function App() {
-   return <></>
-}
+import { useState } from 'react';
+import './App.css';
+import RegistrationForm from './components/RegistrationForm';
+
+const App = () => {
+   const [enteredName, setEnteredName] = useState("");
+   return (
+   <div>
+      <h1>Hello, {enteredName}</h1>
+      <input
+      onChange = {(e) => {
+         setEnteredName(e.target.value);
+      }}
+      type = "text"
+      placeholder='Your name...'
+      />
+      <RegistrationForm />
+   </div>
+   );
+};
 
 export default App;
