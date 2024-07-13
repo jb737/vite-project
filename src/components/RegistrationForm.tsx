ef0d78ce3240cf072475d7c26b0610f02f688651
J Friesen <jiffybugg2@gmail.com>
2024-07-13 17:28:06 -0600

    update registration form

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,31 +1,43 @@
-//this code was written by Ygor Futado. I copied it from class to learn best practices. Mine was functional, not great
+//this code was written by Ygor Futado. I copied it from class to learn best practices. Mine was functional but did not use best practices. How embarassing... my inexperience is showing!
 import React, { useState } from "react";
+import Student from "../models/Student";
+import GRADE_LEVELS from "../constants/gradeLevels";
 
 export default function RegistrationForm() {
-    const [firstName, setFirstName] = useState<string>("");
-    const [lastName, setLastName] = useState<string>("");
-    const [age, setAge] = useState<string>("");
+    const [student, setStudent] = useState<Student>({
+        firstName: "",
+        lastName: "",
+        age: 0,
+        gradeLevel: {
+            id: 0,
+            name: "",
+        },
+    });
+//I have created a student object, I have assigned it to use the Student.ts contract, to get our constant
     return (
-    <React.Fragment>
+    <section>
+        <h3>New Student Registration Form</h3>
             <form>
-                <input onChange = {(event) => setFirstName(event.target.value)} 
+                <input onChange = {(event) => setStudent(mostRecentState => ({...mostRecentState, firstName: event.target.value,}))} 
                     type = "text" 
                     placeholder = "First Name"
-                    value = {firstName}
+                    value = {student.firstName}
                     />
-                <input onChange = {(event) => setLastName(event.target.value)} 
+                <input onChange = {(event) => setStudent(mostRecentState => ({...mostRecentState, lastName: event.target.value,}))}  
                     type = "text" 
                     placeholder = "Last Name"
-                    value = {lastName}
+                    value = {student.lastName}
                     />
-                <input onChange = {(event) => setAge(event.target.value)} 
-                    min = {0} 
+                <input onChange = {(event) => setStudent((mostRecentState) => ({...mostRecentState, age: +event.target.value,}))} 
+                    min = {1} 
                     type = "number" 
                     placeholder = "Age"
-                    value = {age}
+                    value = {student.age}
                     />
+                <select onChange = {(event) => setStudent((mostRecentState) => ({...mostRecentState, gradeLevel: GRADE_LEVELS.find(gl => gl.id === +event.target.value,) || { id: 0, name: ""}}))}>
+                    {GRADE_LEVELS.map((gradeLevel, index) => (<option value={gradeLevel.id} key={index}>{gradeLevel.name}</option>))}
+                </select>
             </form>
-        <p>Hello, {lastName}, {firstName}. Are you sure that you are {age} years old?</p> 
-    </React.Fragment>
-    );
+    </section>
+    );//onChange, it will get the most recent state 
 }
