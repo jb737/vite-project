6994e0da7f0bc47a2b458e291fdf047cddc7749a
J Friesen <jiffybugg2@gmail.com>
2024-07-13 17:27:21 -0600

    create gradelevel interface

---
+++ src/constants/gradeLevels.ts
@@ -0,0 +1,63 @@
+import GradeLevel from "../models/gradeLevelInterface";
+
+ const GRADE_LEVELS: GradeLevel[] = [
+    {
+        id: 1,
+        name: "Kindergarten",
+    },
+    {
+        id: 2,
+        name:  "First Grade",
+    },
+    {
+        id: 3,
+        name:"Second Grade",
+    },
+    {
+        id: 4,
+        name: "Third Grade",
+    },
+    {
+        id: 5,
+        name : "Fourth Grade",
+    },
+    {
+        id: 6,
+        name : "Fifth Grade",
+    },
+    {
+        id: 7,
+        name:  "Sixth Grade",
+    },
+    {
+        id: 8,
+        name : "Seventh Grade",
+    },
+    {
+        id: 9,
+        name:"Eighth Grade",
+    },
+    {
+        id: 10,
+        name : "Ninth Grade",
+    },
+    {
+        id: 11,
+        name:  "Tenth Grade",
+    },
+    {
+        id: 12,
+        name:  "Eleventh Grade",
+    },
+    {
+        id: 13,
+        name:   "Twelfth Grade",
+    },
+    {
+        id: 14,
+        name: "College Freshman",
+    },
+   
+];
+
+export default GRADE_LEVELS;
