9b3cbf9225476ea10a7b7fd16f5f8b28a2b47ba8
J Friesen <jiffybugg2@gmail.com>
2024-07-13 18:04:54 -0600

    add comments for clarity

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -4,6 +4,6 @@ export default interface Student {
     firstName: string;
     lastName: string;
     age: number;
-    gradeLevel: GradeLevel;
-    grades?: number [];
+    gradeLevel: GradeLevel; //this is a nested object
+    grades?: number [];// this is optional
 }
