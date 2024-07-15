5c0cf13065531fcf7567eba491e72804bcc45102
J Friesen <jiffybugg2@gmail.com>
2024-07-15 15:50:25 -0600

    create person interface

---
+++ src/models/person.ts
@@ -0,0 +1,6 @@
+export default interface person {
+    id: number,
+    name: string,
+    age: number,
+    email: string,
+}//id must always be number, name always string, age is number, email string.
